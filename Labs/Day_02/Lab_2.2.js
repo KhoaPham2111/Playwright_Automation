4806477c4467d2696e435e68ce2602f33874b411
Khoa Pham <khoapham.drake2111@gmail.com>
2024-06-24 23:21:36 +0700

    Update Day 02 Labs & Lessions

--- Labs/Day_02/Lab_2.2.js
+++ Labs/Day_02/Lab_2.2.js
@@ -1,8 +1,8 @@
 const readline = require('readline-sync');
 
 let number = readline.question('Please input a number:')
-let decimal = number/2 - Math.floor(number/2)
+let decimal = number / 2 - Math.floor(number / 2)
 console.log(`${decimal}`);
-if (decimal>0){
+if (decimal > 0) {
     console.log('Your number is a odd number');
 } else console.log('Your number is a even number');
