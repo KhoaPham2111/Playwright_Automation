3b231fd46189054dd125c4344d8620f5d7b1336f
Khoa Pham <khoapham.drake2111@gmail.com>
2024-06-24 23:23:33 +0700

    Update Day 02 Labs & Lessions

--- Labs/Day_02/Lab_2.1.js
+++ Labs/Day_02/Lab_2.1.js
@@ -12,9 +12,9 @@ console.log(`Your BMI is: ${BMI}`);
 // Decide User body fatness
 if (BMI <= 18.5) {
     console.log('You are Underweight, you should increase your weight');
-} else if (BMI < 24.9) {
+} else if (BMI <= 24.9) {
     console.log('You are Normal weight, you are doing well, please keep your body fit');
-} else if (BMI < 29.9) {
+} else if (BMI <= 29.9) {
     console.log('You are Overweight, please do more exercises and decrease your weight');
 } else {
     console.log('You are Obesity, please decrease your weight immediately or you will have a heart attack');
