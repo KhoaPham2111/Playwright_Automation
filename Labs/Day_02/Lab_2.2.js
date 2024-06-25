12a42f48ccf578db0cff1d1e71fd1581f8220b91
Khoa Pham <khoapham.drake2111@gmail.com>
2024-06-25 14:21:21 +0700

    Update Day_02 Labs

--- Labs/Day_02/Lab_2.2.js
+++ Labs/Day_02/Lab_2.2.js
@@ -1,13 +1,24 @@
 const readline = require('readline-sync');
 
 // Input number
-let number = readline.question('Please input a number:')
+let number = Number(readline.question('Please input a number:'))
 
 // Check number
-let decimal = number / 2 - Math.floor(number / 2)
+let decimal = Number(number / 2 - Math.floor(number / 2)).toFixed(1)
+console.log(`${number / 2}`);
+console.log(`${Math.floor(number / 2)}`);
 console.log(`${decimal}`);
 
 // Statement
-if (decimal > 0) {
-    console.log('Your number is a odd number');
-} else console.log('Your number is a even number');
+if (decimal == 0) {
+    console.log('Your number is a even number');
+} else if (decimal == 0.5) {
+    console.log('Your number is a even number');
+} else (console.log("Please input a valid interger number"));
+
+
+if (Number.isInteger(number)) {
+    if (number % 2 == 0) {
+        console.log('Your number is a even number');
+    } else (console.log('Your number is a odd number'));
+} else (console.log("Please input a valid interger number"));
