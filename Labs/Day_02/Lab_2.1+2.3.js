add32ef963e53ec94dfa5991c2d87a3ac7238077
Khoa Pham <khoapham.drake2111@gmail.com>
2024-07-01 20:37:28 +0700

    Update Lab 2.1

--- Labs/Day_02/Lab_2.1+2.3.js
+++ Labs/Day_02/Lab_2.1+2.3.js
@@ -7,26 +7,26 @@ let userHeight = Number(readline.question('Please input your height (m):'));
 console.log(`${userHeight}m`);
 if (Number.isNaN(userWeight) || Number.isNaN(userHeight)) {
     console.log('Your input is invalid, please try again');
-    let userWeight = Number(readline.question('Please input your weight (kg):'));
-    let userHeight = Number(readline.question('Please input your height (m):'));
 }
+
 // Calculate BMI
 let BMI = (userWeight / (userHeight ** 2)).toFixed(1);
 console.log(`Your BMI is: ${BMI}`);
 
-// Calculate weight different
-let minIdealWeight = (Math.abs(BMI - 18.5) * (userHeight ** 2)).toFixed(1)
-let maxIdealWeight = Math.round(Math.abs(BMI - 24.9) * (userHeight ** 2)).toFixed(1)
-console.log(minIdealWeight);
-console.log(maxIdealWeight);
+// Calculate weight gap
+let gapMinNormalWeight = (Math.abs(BMI - 18.5) * (userHeight ** 2)).toFixed(1);
+let gapMaxNormalWeight = (Math.abs(BMI - 24.9) * (userHeight ** 2)).toFixed(1);
+console.log(gapMinNormalWeight);
+console.log(gapMaxNormalWeight);
+
 
 // Decide User body fatness
 if (BMI < 18.5) {
-    console.log(`You are Underweight, you should gain from ${minIdealWeight}kg to ${maxIdealWeight}kg`);
+    console.log(`You are Underweight, you should gain atleast from ${gapMinNormalWeight}kg to most ${gapMaxNormalWeight}kg`);
 } else if (BMI <= 24.9) {
     console.log('You are Normal weight, you are doing well, please keep your body fit');
 } else if (BMI <= 29.9) {
-    console.log(`You are Overweight, you should lose from ${maxIdealWeight}kg to ${minIdealWeight}kg`);
+    console.log(`You are Overweight, you should lose atleast from ${gapMaxNormalWeight}kg to most ${gapMinNormalWeight}kg`);
 } else {
-    console.log(`You are Obesity, you should lose from ${maxIdealWeight}kg to ${minIdealWeight}kg`);
+    console.log(`You are Obesity, you should lose atleast from ${gapMaxNormalWeight}kg to most ${gapMinNormalWeight}kg`);
 }
--- Labs/Day_02/Lab_2.2.js
+++ Labs/Day_02/Lab_2.2.js
@@ -5,9 +5,9 @@ let number = Number(readline.question('Please input a number:'))
 
 // 1st solution: Using %
 if (Number.isInteger(number)) {
-    if (number % 2 == 0) {
-        console.log('Your number is a even number');
-    } else (console.log('Your number is a odd number'));
+    if (number % 2 === 0) {
+        console.log(`Your number ${number} is a even number`);
+    } else (console.log(`Your number ${number} is a odd number`));
 } else {
     (console.log("Please input a valid interger number"));
 }
@@ -19,10 +19,17 @@ console.log(`${Math.floor(number / 2)}`);
 console.log(decimal);
 
 if (decimal == 0) {
-    console.log('Your number is a even number');
+    console.log(`Your number ${number} is a even number`);
 } else if (decimal == 0.5) {
-    console.log('Your number is a odd number');
+    console.log(`Your number ${number} is a odd number`);
 } else {
     (console.log("Please input a valid interger number"));
 }
 
+// 3rd solution
+let devideResult = number / 2;
+if (Number.isInteger(devideResult)) {
+    console.log(`Your number ${number} is a even number`);
+} else {
+    console.log(`Your number ${number} is a odd number`);
+}
